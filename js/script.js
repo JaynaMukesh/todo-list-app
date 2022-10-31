720a8e1596b216e9329f9fadd14452750ffd42a4
Webxspark <webxspark@gmail.com>
2022-10-31 16:09:23 +0530

    Title font size reduced

--- js/script.js
+++ js/script.js
@@ -149,8 +149,8 @@ var app = {
                 <div class="row m-1 p-4">
                     <div class="col">
                         <div class="p-1 h1 text-primary text-center mx-auto display-inline-block">
-                            <i class="fa fa-check bg-primary text-white rounded p-2"></i>
-                            <u>My Todo-s</u>
+                            <i class="fa fa-check bg-primary text-white rounded p-2" style="font-size: 30px;"></i>
+                            <u style="font-size: 26px;">My Todo-s</u>
                         </div>
                     </div>
                 </div>
